d8328e54e3132b76197d1a61f3f1fe1c2786300e
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2023-11-23 14:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,33 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+// Function to generate a random timeout promise
+const generateRandomTimeoutPromise = () => {
+  const timeout = Math.floor(Math.random() * 5) + 1; // Random time between 1 and 5 seconds
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise resolved in ${timeout} seconds`);
+    }, timeout * 1000); // Convert seconds to milliseconds
+  });
+};
+
+// Array of five promises
+const promises = [
+  generateRandomTimeoutPromise(),
+  generateRandomTimeoutPromise(),
+  generateRandomTimeoutPromise(),
+  generateRandomTimeoutPromise(),
+  generateRandomTimeoutPromise(),
+];
+
+// Use Promise.any() to wait for the first promise to resolve
+Promise.any(promises)
+  .then((result) => {
+    // Print the result to the div with id 'output'
+    document.getElementById('output').innerText = result;
+  })
+  .catch((error) => {
+    // Handle errors if needed
+    console.error('Error:', error);
+  });
+
